166111e145e37dbdb72f6c9127479b4c7e71688c
Sergio Majluf <sergio.majluf@gmail.com>
2013-11-03 21:47:11 -0500

    Server edit & fix

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var app = express();
  
 app.get('/', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Temperature Monitor App\n');
+	res.end('Temperature Monitor App');
 });
 
 app.get('/save', function(req, res) {
     res.send("Incomplete request. Missing the User & Temp");
 });
+
 app.get('/save/:id', function(req, res) {
     res.send("Incomplete request. Missing Temp");
 });
+
 app.get('/save/:id/:temp', function(req, res) {
     res.send({user:(req.params.id).toUpperCase(), temp: req.params.temp});
     console.log({user:(req.params.id).toUpperCase(), temp: req.params.temp});
